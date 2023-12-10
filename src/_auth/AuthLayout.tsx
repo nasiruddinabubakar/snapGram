5bf6af19c5711d6fd458ddee20a0bcdb6d045824
Nasiruddin Abubakar <111733598+nasiruddinabubakar@users.noreply.github.com>
2023-12-10 17:12:10 +0430

    first commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+        <section className="flex flex-1 justify-center items-center py-10 flex-col">
+          <Outlet />
+        </section>
+
+        <img src="fix.svg"
+        alt="logo"
+        className="hidden xl:block hscreen w-1/2 object-cover bg-no-repeat"
+      />
+      </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
